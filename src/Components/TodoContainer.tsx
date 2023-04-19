cb3c9e91ebe35c070f28d7887e7e5d64bb082bf6
Sagar Mainali <sagarmainali78@gmail.com>
2023-04-19 22:47:10 +0545

    component design final

--- src/Components/TodoContainer.tsx
+++ src/Components/TodoContainer.tsx
@@ -5,14 +5,15 @@ function TodoContainer() {
      return (
           <div className='flex flex-col gap-2'>
                <fieldset className='px-2 mb-2'>
-                    <label>Filter
-                         <select name="filter">
+                    <label>Filter:
+                         <select name="filter" className='ms-2 ps-1 pe-14 py-1 cursor-pointer rounded-md focus:outline-0'>
                               <option value="all">All</option>
                               <option value="done">Done</option>
                               <option value="todo">Todo</option>
                          </select>
                     </label>
                </fieldset>
+               <Todo />
                <Todo />
                <Todo />
           </div>
