74d5d5f5b56319232d5bc7afbf7e1c7dbbafcbb0
Sagar Mainali <sagarmainali78@gmail.com>
2023-04-20 00:07:27 +0545

    function update

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,19 @@ import TodoObj from './todoModel'
 
 function App() {
 
-  const test: TodoObj[] = [
-    {
-      id: 1,
-      todo: 'hello',
-      completed: true,
-      dueDate: '123123'
-    },
-    {
-      id: 1,
-      todo: 'hello',
-      completed: false,
-      dueDate: '123123'
-    }
-  ]
+  const [todo, setTodo] = useState<TodoObj[]>([])
 
-  const [todo, setTodo] = useState<TodoObj[]>(test)
+  function addTodo(): void {
+    console.log('todo added')
+  }
 
   return (
     <div className='py-8 px-2'>
       <div className="container mx-auto bg-graay py-8 px-4 sm:px-12 rounded-lg flex flex-col gap-10">
         <h1 className='text-center text-4xl text-bluey font-semibold underline'>My Todos</h1>
-        <Header />
+        <Header addTodo={addTodo} />
         <hr className='border border-gray-300' />
-        <TodoContainer todo={ todo} />
+        <TodoContainer todo={todo} />
       </div >
     </div>
   )
