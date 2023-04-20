10d56a8d82e14652c175e35282f01ad28f570e49
Sagar Mainali <sagarmainali78@gmail.com>
2023-04-20 11:38:46 +0545

    updated design

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
+
 function Header({ addTodo }: { addTodo: () => void }) {
      return (
-          <div className='flex items-center gap-3 bg-fwhite px-10 py-7 rounded-md shadow-md'>
+          <div className='flex items-center gap-1 sm:gap-3 bg-fwhite px-5 sm:px-8 py-7 rounded-md shadow-md'>
                <input className="flex-1 focus:outline-0 placeholder:text-greyy" type="text" placeholder="Add new..." />
-               <svg className="cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height='20'><path d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z" fill='#3B71CA' /></svg>
-               <button className="bg-bluey text-fwhite py-2 px-6 rounded-md text-xs font-semibold drop-shadow-xl" onClick={() => addTodo()}>ADD</button>
+               {/* <svg className="cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height='20'><path d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z" fill='#3B71CA' /></svg> */}
+               <label htmlFor="date"></label>
+               <input id='date' type="date" className='w-[7.5rem] cursor-pointer focus:outline-0' defaultValue='2023-04-19' min='2023-04-19'/>
+               <button className="bg-bluey text-fwhite py-2 px-6 rounded-md text-xs font-semibold drop-shadow-xl duration-150 hover:scale-105" onClick={() => addTodo()}>ADD</button>
           </div>
      )
 }
--- src/Components/TodoContainer.tsx
+++ src/Components/TodoContainer.tsx
@@ -10,13 +10,12 @@ function TodoContainer({ todo }: { todo: TodoObj[] }) {
      return (
           <div className='flex flex-col gap-2'>
                <fieldset className='px-2 mb-2'>
-                    <label>Filter:
-                         <select name="filter" className='ms-2 ps-1 pe-14 py-1 cursor-pointer rounded-md focus:outline-0'>
-                              <option value="all">All</option>
-                              <option value="done">Done</option>
-                              <option value="todo">Todo</option>
-                         </select>
-                    </label>
+                    <label>Filter:</label>
+                    <select name="filter" className='ms-2 ps-1 pe-14 py-1 cursor-pointer rounded-md focus:outline-0'>
+                         <option value="all">All</option>
+                         <option value="done">Done</option>
+                         <option value="todo">Todo</option>
+                    </select>
                </fieldset>
                {todo.map((item: TodoObj) => <Todo key={item.id} item={item} />)}
           </div>
