64a467822a693491b31ad06091d7f46a81071a4a
Sagar Mainali <sagarmainali78@gmail.com>
2023-04-21 23:46:26 +0545

    using useRef and useContext hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
+import { createContext, useState } from 'react'
 import Header from './Components/Header'
 import TodoContainer from './Components/TodoContainer'
 import TodoObj from './todoModel'
+
+export const FunctionContext = createContext()
 
 function App() {
 
   const [todo, setTodo] = useState<TodoObj[]>([])
 
-  function removeTodo(id: number) {
 
+  function removeTodo(id: number) {
+    console.log('deleted')
   }
 
   function editTodo(id: number) {
@@ -16,20 +19,17 @@ function App() {
   }
 
   return (
-    <div className='py-8 px-2 md:px-20'>
-      <div className="container mx-auto bg-graay py-8 px-4 sm:px-12 rounded-lg flex flex-col gap-10">
-        <h1 className='text-center text-4xl text-bluey font-semibold underline'>My Todos</h1>
-        <Header setTodo={setTodo} />
-        <hr className='border border-gray-300' />
-        <TodoContainer todo={todo} />
-      </div >
-    </div>
+    <FunctionContext.Provider value={removeTodo} >
+      <div className='py-8 px-2 md:px-20'>
+        <div className="container mx-auto bg-graay py-8 px-4 sm:px-12 rounded-lg flex flex-col gap-10">
+          <h1 className='text-center text-4xl text-bluey font-semibold underline'>My Todos</h1>
+          <Header setTodo={setTodo} />
+          <hr className='border border-gray-300' />
+          <TodoContainer todo={todo} />
+        </div >
+      </div>
+    </FunctionContext.Provider>
   )
 }
 
 export default App
-
-// todo
-// display message if input value is null
-// add edit functionality
-// add delete functionality
