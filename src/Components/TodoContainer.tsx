ba109e6d3bb2624d7ec38bd5a6c05fe8eb71c013
Sagar Mainali <sagarmainali78@gmail.com>
2023-04-23 15:29:29 +0545

    added filter functionality

--- src/Components/TodoContainer.tsx
+++ src/Components/TodoContainer.tsx
@@ -10,29 +10,45 @@ function TodoContainer({ todos }: { todos: TodoObj[] }) {
 
      const [selectedOption, setSelectedOption] = useState<string>('all')
 
-     function changeSelectedOption(value: string) {
-          if (value === 'completed') { 
-               const completedTodos = [...todos]
-          }
-          setSelectedOption(value)
-     }
+     // get completed todos only
+     const completedTodos = todos.filter(
+          (item: TodoObj) => (
+               item.checked && item
+          )
+     )
+
+     // get incompleted todos
+     const incompletedTodos = todos.filter(
+          (item: TodoObj) => (
+               !item.checked && item
+          )
+     )
 
      return (
           <div className='flex flex-col gap-2'>
                <fieldset className='px-2 mb-4'>
                     <label>Filter:</label>
-                    <select name="filter" className='ms-2 ps-1 pe-14 py-1 cursor-pointer rounded-md focus:outline-0' value={selectedOption} onChange={(e) => changeSelectedOption(e.target.value)}>
+                    <select name="filter" className='ms-2 ps-1 pe-14 py-1 cursor-pointer rounded-md focus:outline-0' value={selectedOption} onChange={(e) => setSelectedOption(e.target.value)}>
                          <option value="all">All</option>
                          <option value="completed">Completed</option>
                          <option value="todos">Todos</option>
                     </select>
                </fieldset>
-               {todos.length >= 1
+               {selectedOption === 'all' && todos.length >= 1
                     ? todos.map((item: TodoObj) => <Todo key={item.id} item={item} />)
-                    : <h2 className='text-center py-6 text-lg'>There are no <span className='text-bluey font-semibold '>todos</span> at the moment</h2>
+                    : selectedOption === 'all' && todos.length < 1
+                         ? <h2 className='text-center py-6 text-lg'>There are no any <span className='text-bluey font-semibold '>todos</span> at the moment</h2>
+                         : selectedOption === 'completed' && completedTodos.length >= 1
+                              ? completedTodos.map((item: TodoObj) => <Todo key={item.id} item={item} />)
+                              : selectedOption === 'completed' && completedTodos.length < 1
+                                   ? <h2 className='text-center py-6 text-lg'>No completed <span className='text-bluey font-semibold '>todos</span> as of now</h2>
+                                   : selectedOption === 'todos' && incompletedTodos.length >= 1
+                                        ? incompletedTodos.map((item: TodoObj) => <Todo key={item.id} item={item} />)
+                                        : <h2 className='text-center py-6 text-lg'>No remaining <span className='text-bluey font-semibold '>todos</span> as of now</h2>
                }
           </div>
      )
 }
 
 export default TodoContainer
+
